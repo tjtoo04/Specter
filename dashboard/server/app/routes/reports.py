431f3e0a26d861ff91cf447e6f304737ff70b6af
Tze Jiat <ttjiat@gmail.com>
2026-02-08 10:51:15 +0800

    update url

--- dashboard/server/app/routes/reports.py
+++ dashboard/server/app/routes/reports.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models.models import Report
 
-router = APIRouter(prefix="/api/projects", tags=["projects"])
+router = APIRouter(prefix="/api/reports", tags=["reports"])
 
 
-@router.post("/reports")
+@router.post("/")
 async def create_report(file: UploadFile = File(...), db: Session = Depends(get_db)):
     file_content = await file.read()
     new_report = Report(data=file_content)
